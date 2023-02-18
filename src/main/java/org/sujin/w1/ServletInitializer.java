03bd551dbc8d07fc29e8395a8aeed5c6d384e345
SUJINJEONG012 <peekaboo32@naver.com>
2023-02-19 03:12:06 +0900

    자바웹MVC패턴으로 기본을 다시 공부중

---
+++ src/main/java/org/sujin/w1/ServletInitializer.java
@@ -0,0 +1,13 @@
+package org.sujin.w1;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(W1Application.class);
+	}
+
+}
